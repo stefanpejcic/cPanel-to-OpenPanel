9bebb20dd69c43a20321e714748d88781a74054a
Stefan Pejcic <stefan@pejcic.rs>
2024-06-12 15:03:58 +0200

    Update cpanel-import.py

--- cpanel-import.py
+++ cpanel-import.py
@@ -1,1 +1,23 @@
-# module code for OpenAdmin
+import os
+import json
+import socket
+from flask import Flask, Response, abort, render_template, request, send_file, g, jsonify, session, url_for, flash, redirect, get_flashed_messages
+import subprocess
+import datetime
+import psutil
+from app import app, is_license_valid, login_required_route, load_openpanel_config, connect_to_database
+import docker
+
+from modules.helpers import get_all_users, get_user_and_plan_count, get_plan_by_id, get_all_plans, get_userdata_by_username, get_hosting_plan_name_by_id, get_user_websites, is_username_unique, gravatar_url
+
+# 
+@app.route('/import/cpanel', methods=['GET', 'POST'])
+@login_required_route
+def import_cpanel_whm_account():
+    if request.method == 'POST':
+        # this will be the actual exec
+        return redirect('/settings/docker')
+    else:
+        return render_template('cpanel-import.html', title='Import cPanel account')
+
+# helper funcitons to verify
