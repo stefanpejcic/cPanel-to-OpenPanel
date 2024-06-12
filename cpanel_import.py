0e7ca8d02b5d46d7185dc80558e1fa82b7c9f1e4
Stefan Pejcic <stefan@pejcic.rs>
2024-06-12 16:24:17 +0200

    Update cpanel_import.py

--- cpanel_import.py
+++ cpanel_import.py
@@ -1,14 +1,10 @@
 import os
-import json
+import json # will use for get to return data
 import socket
 from flask import Flask, Response, abort, render_template, request, send_file, g, jsonify, session, url_for, flash, redirect, get_flashed_messages
 import subprocess
-import datetime
-import psutil
-from app import app, is_license_valid, login_required_route, load_openpanel_config, connect_to_database
-import docker
-
-from modules.helpers import get_all_users, get_user_and_plan_count, get_plan_by_id, get_all_plans, get_userdata_by_username, get_hosting_plan_name_by_id, get_user_websites, is_username_unique, gravatar_url
+from app import app, is_license_valid, login_required_route
+from modules.helpers import get_all_plans, is_username_unique
 
 @app.route('/import/cpanel', methods=['GET', 'POST'])
 @login_required
