73d0fc242dd7481c7d2568b456aba646bb5a1d65
Stefan Pejcic <stefan@pejcic.rs>
2024-06-12 16:16:12 +0200

    Update cpanel_import.py

--- cpanel_import.py
+++ cpanel_import.py
@@ -10,12 +10,26 @@ import docker
 
 from modules.helpers import get_all_users, get_user_and_plan_count, get_plan_by_id, get_all_plans, get_userdata_by_username, get_hosting_plan_name_by_id, get_user_websites, is_username_unique, gravatar_url
 
-# 
 @app.route('/import/cpanel', methods=['GET', 'POST'])
-@login_required_route
+@login_required
 def import_cpanel_whm_account():
     if request.method == 'POST':
+        path = request.form.get('path')
+        plan_name = request.form.get('plan_name')
+
+        if not path or not plan_name:
+            flash('Path and Plan name are required!', 'error')
+            return redirect('/import/cpanel')
+        try:
+            result = subprocess.run(['opencli', 'user-import', 'cpanel', path, plan_name], capture_output=True, text=True)
+            # example: opencli user-import cpanel "/home/backups/cpmovefile_02512024.tar.gz" default_plan_nginx
+            if result.returncode == 0:
+                flash('Import started!', 'success')
+            else:
+                flash(f'Import failed: {result.stderr}', 'error')
+        except Exception as e:
+            flash(f'An error occurred: {str(e)}', 'error')
+
         return redirect('/import/cpanel')
     else:
         return render_template('cpanel-import.html', title='Import cPanel account')
-
