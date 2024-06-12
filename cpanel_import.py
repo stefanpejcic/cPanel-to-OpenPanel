1d58b58f7ce68a1fceb5ed8102755f3f663f9750
Stefan Pejcic <stefan@pejcic.rs>
2024-06-12 16:22:59 +0200

    Update cpanel_import.py

--- cpanel_import.py
+++ cpanel_import.py
@@ -18,18 +18,23 @@ def import_cpanel_whm_account():
         plan_name = request.form.get('plan_name')
 
         if not path or not plan_name:
-            flash('Path and Plan name are required!', 'error')
+            flash('Both path to the cPanel backup file (.tar.gz) and plan name are required!', 'error')
             return redirect('/import/cpanel')
+
         try:
-            result = subprocess.run(['opencli', 'user-import', 'cpanel', path, plan_name], capture_output=True, text=True)
-            # example: opencli user-import cpanel "/home/backups/cpmovefile_02512024.tar.gz" default_plan_nginx
-            if result.returncode == 0:
-                flash('Import started!', 'success')
-            else:
-                flash(f'Import failed: {result.stderr}', 'error')
+            file_name = os.path.basename(path)
+            log_file_name = f"cpanel_import_log_{os.path.splitext(file_name)[0]}"
+            log_file_path = f"/var/log/openpanel/admin/{log_file_name}.log"
+
+            # Run the subprocess command and redirect stdout and stderr to the log file
+            with open(log_file_path, 'w') as log_file:
+                subprocess.Popen(['opencli', 'user-import', 'cpanel', path, plan_name], stdout=log_file, stderr=log_file)
+            
+            flash(f'Import started! To track the progress open the log file: {log_file_path}', 'success')
         except Exception as e:
             flash(f'An error occurred: {str(e)}', 'error')
 
         return redirect('/import/cpanel')
     else:
+        # on GET we will list the sessions in progress..
         return render_template('cpanel-import.html', title='Import cPanel account')
