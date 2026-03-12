101052ef7ca9bfce533c664e5ed5acbc4a272dea
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-12 19:06:08 +0100

    Update cp-import.sh

--- cp-import.sh
+++ cp-import.sh
@@ -557,7 +557,7 @@ restore_files() {
     du_needed_for_home=$(du -sh "$real_backup_files_path/public_html" | cut -f1)
     log "Restoring home directory ($du_needed_for_home) to html_data volume"
     mkdir -p /home/$cyberpanel_username/docker-data/volumes/${cpanel_username}_html_data/
-    rm -rf "$real_backup_files_path"/public_html/{.cpanel,.trash,wordpress-backups}
+    rm -rf "$real_backup_files_path/public_html/.trash"
     mv $real_backup_files_path/public_html /home/$cyberpanel_username/docker-data/volumes/${cpanel_username}_html_data/_data
 }
 
