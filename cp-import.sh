60353ba0f6418ae33a50aeace925f3c17b314e38
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-12 19:03:51 +0100

    Update cp-import.sh

--- cp-import.sh
+++ cp-import.sh
@@ -534,6 +534,7 @@ restore_ssl() {
                 cp "$cert_file" "$new_cert_file"             
                 log "Installing SSL certificate for domain: $domain"
                 opencli domains-ssl "$domain" custom "/var/www/html/$domain.key" "/var/www/html/$domain.crt"
+				rm -rf "$new_cert_file" "$new_key_file"
             else
                 log "SSL key file not found for domain: $domain"
             fi
