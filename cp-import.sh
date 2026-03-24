c62e992092581f66528cce4a146f1ccebbd9b0ee
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-24 18:32:09 +0100

    Update cp-import.sh

--- cp-import.sh
+++ cp-import.sh
@@ -1185,7 +1185,9 @@ ${email}|{SHA512-CRYPT}${password_hash}
 				fi
 				# cpanel storage: extract/backup-3.24.2026_14-03-06_stefantestira/homedir/mail/stefantestira.rs/emailtest2
 				if [ -d "$base_dir/mail/$domain/$username" ]; then
-					rsync -av --remove-source-files "$base_dir/mail/$domain/$username/." "$STORE_EMAILS_IN/$domain/$username/"
+				rm -rf "$STORE_EMAILS_IN/$domain/$username/" && mv "$base_dir/mail/$domain/$username/" "$STORE_EMAILS_IN/$domain/$username/"
+				#TEMPORARY
+				#	rsync -av --remove-source-files "$base_dir/mail/$domain/$username/." "$STORE_EMAILS_IN/$domain/$username/"
 					#rsync -av --remove-source-files "/home/stefantestira/docker-data/volumes/stefantestira_html_data/_data/mail/stefantestira.rs/emailtest1/." "/var/mail/stefantestira.rs/emailtest1/"
 				fi
 	        else
@@ -1195,7 +1197,6 @@ ${email}|{SHA512-CRYPT}${password_hash}
 	        log "Skipping $dir_path (shadow file missing or empty)"
 	    fi
     done
-	find "$base_dir/" -type d -empty -delete
 }
 
 
