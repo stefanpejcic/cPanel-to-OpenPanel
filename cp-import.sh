7e9ce970e1460e30808bdc16f602f4bb8e6a8d2c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-24 18:18:29 +0100

    Update cp-import.sh

--- cp-import.sh
+++ cp-import.sh
@@ -521,7 +521,7 @@ restore_mysql() {
 	if [ -f "${real_backup_files_path}/mysql_host_notes.json" ]; then
 		key_count=$(jq 'keys | length' "${real_backup_files_path}/mysql_host_notes.json")
 		if [ "$key_count" -gt 0 ]; then
-			log "'Remote Database Access' has $key_count hosts - enabling remote mysql access"
+			log "Enabling remote MySSQL access ($key_count hosts detected)"
 			sed -i '/^MYSQL_PORT=/ s/127\.0\.0\.1://g' /home/$cpanel_username/.env
 		fi
 	fi
